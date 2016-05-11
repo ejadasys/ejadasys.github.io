d6ff72e3d6e03e89085aa2f6739054267b5b007d
ejadasys <graphics.samak@gmail.com>
2016-05-11 22:40:24 +0300

    js commit

--- fusion.js
+++ fusion.js
@@ -13,7 +13,9 @@ jQuery(document).ready(function($){
         $(this).parents('.iconBlock').toggleClass('active');
     })
     //$('span.x1fa').text($('span.x1fa').text())
-   alert('text')
+   $('button').each(function(){
+       alert($(this).text())
+   })
 
 
 });
